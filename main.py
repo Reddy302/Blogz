29b2dcd140d7650a6ae08bb753754146d45eb7ee
Reddy <lewismd3283@gmail.com>
2019-03-02 20:39:45 -0600

    created bare bones of routes: default, blog, and newpost. Still have to make it work

--- main.py
+++ main.py
@@ -3,44 +3,54 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    blog_title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
+    def __init__(self, blog_title, body):
+        self.blog_title = blog_title
+        self.body = body
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return render_template('base.html')
+
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
+            blog_title = request.form['blog_title']
+            new_blog = Blog(blog_title, body)
+            db.session.add(new_blog)
+            db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    blogs = Blog.query.all()
+    return render_template('blog.html', title="Build a Blog", blogs=blogs)
+    
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    if request.method == 'GET':
+        # TODO render template and un-indent the return statement
+        return render_template('newpost.html', title="Add a Blog Entry")
 
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
+# @app.route('/delete-task', methods=['POST'])
+# def delete_task():
+#     task_id = int(request.form['task-id'])
+#     task = Task.query.get(task_id)
+#     task.completed = True
+#     db.session.add(task)
+#     db.session.commit()
 
-    return redirect('/')
+#     return redirect('/')
 
 
 if __name__ == '__main__':
